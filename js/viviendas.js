819438723e9bcac1c32083539e537c1d6af893f0
Carlos Díaz Pérez <cdperez.85@gmail.com>
2017-05-22 20:17:23 +0200

    carlos: acceso a listado de viviendas

---
+++ js/viviendas.js
@@ -0,0 +1,18 @@
+
+
+var viviendas = [
+    { ciudad:"AAAAAA", cp:0,comunidad:"cmancha", calle: "AAAAAA", numero: 0, anocons:0,nhabitaciones: 0,precio:0,descripcion:"Lorem ipsum dolor amet o algo así"},
+    { ciudad:"AAAAAA", cp:0,comunidad:"cmancha", calle: "AAAAAA", numero: 0, anocons:0,nhabitaciones: 0,precio:0,descripcion:"Lorem ipsum dolor amet o algo así"},
+    { ciudad:"AAAAAA", cp:0,comunidad:"cmancha", calle: "AAAAAA", numero: 0, anocons:0,nhabitaciones: 0,precio:0,descripcion:"Lorem ipsum dolor amet o algo así"},
+    { ciudad:"AAAAAA", cp:0,comunidad:"cmancha", calle: "AAAAAA", numero: 0, anocons:0,nhabitaciones: 0,precio:0,descripcion:"Lorem ipsum dolor amet o algo así"},
+    { ciudad:"AAAAAA", cp:0,comunidad:"cmancha", calle: "AAAAAA", numero: 0, anocons:0,nhabitaciones: 0,precio:0,descripcion:"Lorem ipsum dolor amet o algo así"},
+    { ciudad:"AAAAAA", cp:0,comunidad:"cantabria", calle: "AAAAAA", numero: 0, anocons:0,nhabitaciones: 0,precio:0,descripcion:"Lorem ipsum dolor amet o algo así"}];
+
+function getPisos(comunidad)
+{
+    
+    for (var i=0;i++;i<viviendas.length )
+    
+    
+    
+}
