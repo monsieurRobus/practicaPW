f984be0f4be8d743b31fda21c00fe4c5fbf7c2b8
Unknown <cdperez.85@gmail.com>
2017-05-27 00:55:42 +0200

    carlos: update ordenacion lista

--- js/func.js
+++ js/func.js
@@ -10,6 +10,7 @@ function getPisos(comunidad)
     contenido='<h1>LISTADO DE VIVIENDAS</h1><div class="menulista"><span>Precio</span><input id="precioup" type="button" value="&uarr;"><input id="preciodown" type="button" value="&darr;"></div><div class="menulista"><span>N Hab.</span><input id="habitacionup" type="button" value="&uarr;"><input id="habitaciondown" type="button" value="&darr;"></div><div class="menulista"><span>Año fab</span><input id="anioup" type="button" value="&uarr;"><input id="aniodown" type="button" value="&darr;"></div><div class="menulista"><span>Interes</span><input id="interesup" type="button" value="&uarr;"><input id="interesdown" type="button" value="&darr;"></div>';          
     
     
+    contenido+="<div id='listapisos'>";
     
     // En esta cadena iremos añadiendo los elementos de la lista para luego pasarselo a la web
     for (var index in stockViviendas)
@@ -21,6 +22,8 @@ function getPisos(comunidad)
                     contenido+='<a href="vistadetalle.html"><div id="'+stockViviendas[index].idAnuncio+'" class="piso" onclick="detalleVista('+stockViviendas[index].idAnuncio+');"><img src="imagenes/'+stockViviendas[index].urlImg+'"><div class="fila"><span id="'+stockViviendas[index].idAnuncio+'" class="col1d4">Vivienda en '+stockViviendas[index].ciudad+'</span><span class="precio col1d4">'+stockViviendas[index].precio+'</span><span class="ninteresados">'+stockViviendas[index].interesados+'</span></div><div class="fila"><p class="descripcion col4d4">'+stockViviendas[index].descripcion+'</p></div><div class="detalles"><span>Año construcción:</span><span class="anocons">'+stockViviendas[index].anocons+'</span></div></div></a>';    
                 }
         }    
+    
+    contenido+='</div>';
     return contenido;   // Devolvemos el contenido de la cadena que hemos construido en el array
 }
 
@@ -57,9 +60,49 @@ function enviarComentario() {
     divForm.innerHTML = contenido;
 }
 
-// Funcion para ordenar. Recibe la variable en función 
+// Funcion para ordenar. Recibe la variable en función de la cual ordenaremos el array y si hacerlo en sentido ascendente o descendente
 function orden(variable,direccion){
     
-    alert(variable+" "+direccion);
+    var comparador='';
+    
+    switch(variable)
+            {
+                case "precio":
+
+                    
+
+                break;
+
+                case "habitacion":
+
+
+
+                break;
+
+                case "anio":
+
+
+
+                break;
+
+                case "interesados":
+
+
+
+                break;
+
+            default:
+            break;
+    }
+    
+        if (direccion)
+                {
+                    
+                }
+            else
+                {
+                    
+                }
+            
     
 }
